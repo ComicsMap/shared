5dfa8b026c009dae682e26b06341b31c145ff392
Clément <clementfiesta@gmail.com>
2026-05-17 21:08:09 +0200

    refactor: Simplify exports in index.ts by using wildcard exports

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,26 +1,9 @@
-import { AuditableEntity } from '@entities/auditable.entity';
-import { IssueLayout } from '@entities/issue-layout.entity';
-import { Cover } from '@entities/issues/cover.entity';
-import {
-  ContributorRole,
-  IssueContributor,
-} from '@entities/issues/issue-contributor.entity';
-import { Issue, Publisher } from '@entities/issues/issue.entity';
-import { Person } from '@entities/person.entity';
-import { ReadingEdge } from '@entities/reading-edge.entity';
-import { Series } from '@entities/series.entity';
-import { User } from '@entities/users/user.entity';
-
-export {
-  AuditableEntity,
-  ContributorRole,
-  Cover,
-  Issue,
-  IssueContributor,
-  IssueLayout,
-  Person,
-  Publisher,
-  ReadingEdge,
-  Series,
-  User,
-};
+export * from '@entities/auditable.entity';
+export * from '@entities/issue-layout.entity';
+export * from '@entities/issues/cover.entity';
+export * from '@entities/issues/issue-contributor.entity';
+export * from '@entities/issues/issue.entity';
+export * from '@entities/person.entity';
+export * from '@entities/reading-edge.entity';
+export * from '@entities/series.entity';
+export * from '@entities/users/user.entity';
