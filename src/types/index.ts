93d11dd16ee7e3f8d48b16e3b0006aac5bf75453
Clément <clementfiesta@gmail.com>
2026-05-17 08:38:33 +0200

    feat: Refactor type definitions and update package exports for better module resolution

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,2 @@
-declare global {
-  type Nullable<T> = T | null;
-  type Optional<T> = T | undefined;
-  type Nullish<T> = T | null | undefined;
-}
-
 export * from '@/types/sessions.types';
 export * from '@/types/users.types';
